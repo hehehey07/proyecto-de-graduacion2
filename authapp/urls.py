825c76a8b32781b39bbe670890f29db1b5cfb9e0
luis <luisquinto29@gmail.com>
2024-10-25 12:00:00 -0600

    inicio de sesion

---
+++ authapp/urls.py
@@ -0,0 +1,9 @@
+# authapp/urls.py
+from django.urls import path
+from .views import register_view, login_view, logout_view
+app_name = 'authapp'
+urlpatterns = [
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+]
